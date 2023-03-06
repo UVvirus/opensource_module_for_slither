56eeecf4473f8b0cd7454487fe58c02658389d61
UVvirus <39599249+UVvirus@users.noreply.github.com>
2023-03-06 13:13:13 +0530

    Update private_variable_reader.py

--- private_variable_reader.py
+++ private_variable_reader.py
@@ -17,7 +17,7 @@ def read_contract(filename: str):
                     print("variable list:", variable_list)
 
     datatype= parse_datatype(variable_list)
-    write_contract("TestConstant.sol",datatype,"BYTES")
+    write_contract("TestConstant.sol",datatype)
 
 # Now list_of_lines variable is like this
 #   [ bytes32 public constant BYTES =\n', '\n', uint256 public someUint256 = 3;\n', ]
@@ -28,18 +28,20 @@ def parse_datatype(list_of_lines:list):
         split=elements.split(' ')
         if len(split) > 1:
             datatype_list.append(split[0])
-    print("datatype list:",datatype_list)
+            #tuple(datatype_list)
+    print("datatype list:",list(set(datatype_list)))
     return datatype_list
 
 def parse_variable_name():
     pass
 
-def write_contract(filename: str, datatype: list, variable_name: str):
+def write_contract(filename: str, datatype: list):
     with open(filename, 'a') as write_smart_contract:
-        getter_method = f'function read_private_variable() public view returns({datatype})' \
-                        f'{{return {variable_name}; ' \
-                        f'}}'
-        write_smart_contract.write(getter_method)
+        for i in range(len(datatype)):
+            getter_method = f'function read_{datatype[i]}_private_variable({datatype[i]} variable_name) public view returns({datatype[i]})' \
+                            f'{{return variable_name; ' \
+                            f'}}'
+            write_smart_contract.write(getter_method)
     write_smart_contract.close()
 
 
